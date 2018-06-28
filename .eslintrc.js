b11a58a90cd4a5a6d935d08764c7e38a3b2f1dd0
Isaac Lo <isaaclo123@gmail.com>
2018-06-28 12:46:51 -0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
     "node": true,
   },
   "rules": {
+    // problem with @ alias
+    'import/no-unresolved':'off',
+    // don't require extensions when importing
+    'import/extensions': 'off',
     //added to prevent unexpected console statement error
     'no-console':'off',
   },
