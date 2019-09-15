7417e0365095ef0be6d1147658da09be49c51449
isaaclo123 <isaaclo123@gmail.com>
2019-09-15 01:21:10 -0500

    changes to projects and resume page

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -1,6 +1,7 @@
 import Slides from '@/js/slides';
 import loadImage from '@/js/image';
 
+import nixos from '@/pages/projects/nixos.png';
 import kanetus from '@/pages/projects/kanetuslogo.png';
 import reddit from '@/pages/projects/reddit.png';
 import sol from '@/pages/projects/solcircle.png';
@@ -10,6 +11,7 @@ require('@/pages/projects/projects.scss');
 
 export default () => {
   window.slides = new Slides();
+  loadImage(nixos, 'nixos');
   loadImage(kanetus, 'kanetus');
   loadImage(reddit, 'reddit');
   loadImage(sol, 'sol');
